848acf63fed9627020a0dff6e2a9a1974de571f6
CircleKEgypt <circlekegyptit@gmail.com>
2024-02-13 09:30:00 +0200

    g

--- CK/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CK.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- CK/Models/VMLogin.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CK.Models
-{
-    public class VMLogin
-    {
-        public string username { get; set; }
-        public string Password { get; set; }
-
-    }
-}
